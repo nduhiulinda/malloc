9327e6f11ed4c0673e1740c2287aec5952cb2c0f
Linda Wanjiru Nduhiu <lwn3@cornell.edu>
2020-05-18 19:59:02 -0400

    print debug

--- release/heaplib.c
+++ release/heaplib.c
@@ -26,7 +26,7 @@ void print_debug_heap_header(heap_header_t *header) {
 		   "heap_size = %d\n",            // Notice: no commas between lines
 		   header, header->heap_size); 
 
-	printf("[%d] = [%d,%d,%d]\n", 0, header->blocks[0].block_size, header->blocks[0].allocated);
+	printf("block[%d] =[%d,%d]\n", 0, header->blocks[0].block_size, header->blocks[0].allocated);
 
 #endif
 }
