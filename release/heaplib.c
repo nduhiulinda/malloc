c0cb88887893bbc9f1c89cb845001dabce697a7a
Osman Napari Alidu <ona5@cornell.edu>
2020-05-21 16:45:16 -0400

    flush

--- release/heaplib.c
+++ release/heaplib.c
@@ -84,6 +84,7 @@ block_info_t* find_block(heap_header_t *header, void *block, int block_size) {
            return curr_block;
        }
        printf("block size:%d\n",curr_block->block_size);
+       fflush(NULL);
        i+=curr_block->block_size;
        curr_block=ADD_BYTES(curr_block, curr_block->block_size);
        if ((uintptr_t)curr_block%ALIGNMENT!=0){
