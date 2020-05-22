25cb176d9dc6121051bbb17e2bb7bff8eb67ba66
Linda Wanjiru Nduhiu <lwn3@cornell.edu>
2020-05-22 18:20:26 -0400

    add padding to  prev block size

--- release/heaplib.c
+++ release/heaplib.c
@@ -169,6 +169,7 @@ void *hl_alloc(void *heap, unsigned int block_size) {
                 if ((uintptr_t)new_block%ALIGNMENT!=0){
                   int rem = (uintptr_t)new_block%ALIGNMENT;
                   new_block=ADD_BYTES(new_block,(ALIGNMENT-rem));
+                  curr_block->block_size +=ALIGNMENT-rem;
                 }
                 new_block->block_size= old_size - curr_block->block_size;
                 printf("(alloc realign new alloc) realigned new_block:%p\n",new_block);
