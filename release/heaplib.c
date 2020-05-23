c897383e5e9acf5ce1c53154acd2e199daf61917
Linda Wanjiru Nduhiu <lwn3@cornell.edu>
2020-05-22 23:41:20 -0400

    compile

--- release/heaplib.c
+++ release/heaplib.c
@@ -324,7 +324,7 @@ void *hl_resize(void *heap, void *block, unsigned int new_size) {
             memmove(new_block,ADD_BYTES(finder,sizeof(block_info_t)), sizeof(char)*ori_new_size);
             hl_release2(heap,ADD_BYTES(finder,-sizeof(block_info_t)));
             mutex_unlock(&malloc_lock); 
-            return ADD_BYTES(new_block);
+            return new_block;
         }
     }
     mutex_unlock(&malloc_lock);
